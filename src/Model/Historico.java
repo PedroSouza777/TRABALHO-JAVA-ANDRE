a7612bda2556e1c2a9339cec643bc77827dc54a1
igor <igordiasr02@gmail.com>
2026-06-19 00:28:59 -0300

    histórico

--- src/Model/Historico.java
+++ src/Model/Historico.java
@@ -1,4 +1,33 @@
-package Model;
+package model;
 
-public class Historico {
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Historico extends EntidadeBase {
+    private static final long serialVersionUID = 1L;
+
+    private String usuarioEmail;
+    private String acao;
+    private LocalDateTime data;
+
+    public Historico(String usuarioEmail, String acao) {
+        super("Historico");
+        this.usuarioEmail = usuarioEmail;
+        this.acao = acao;
+        this.data = LocalDateTime.now();
+    }
+
+    public String getUsuarioEmail() {
+        return usuarioEmail;
+    }
+
+    public String getAcao() {
+        return acao;
+    }
+
+    @Override
+    public String resumo() {
+        String dataFormatada = data.format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm"));
+        return "#" + getId() + " " + dataFormatada + " | " + usuarioEmail + " | " + acao;
+    }
 }
