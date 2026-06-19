51e46ca1324efd165519f8f42e00f92896286b2f
igor <igordiasr02@gmail.com>
2026-06-19 00:33:14 -0300

    histórico

--- src/Controller/HistoricoController.java
+++ src/Controller/HistoricoController.java
@@ -1,4 +1,21 @@
-package Controller;
+package controller;
+
+import model.BancoDados;
+import model.Historico;
+import view.HistoricoView;
 
 public class HistoricoController {
+    private final BancoDados banco;
+    private final HistoricoView view;
+
+    public HistoricoController(BancoDados banco, HistoricoView view) {
+        this.banco = banco;
+        this.view = view;
+    }
+
+    public void listarHistorico() {
+        for (Historico historico : banco.getHistoricos()) {
+            view.mostrarHistorico(historico);
+        }
+    }
 }
